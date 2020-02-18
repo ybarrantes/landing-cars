dd5d71e5edbe14e95c6e8e8826945168a8313f08
Yonathan Gustavo Barrantes Leon <ybarrantes.juntos085@gmail.com>
2020-02-18 18:53:40 -0500

    agrego tablas del modelo

---
+++ database/migrations/2020_02_18_232953_create_departamentos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepartamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departamentos', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->unsignedSmallInteger('codigo');
+            $table->string('nombre', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departamentos');
+    }
+}
