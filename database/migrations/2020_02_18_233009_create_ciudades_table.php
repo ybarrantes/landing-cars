dd63fbba651fb323d24b9222de9ab5cb6d050bef
Yonathan Gustavo Barrantes Leon <ybarrantes.juntos085@gmail.com>
2020-02-18 19:19:17 -0500

    agregar valores por defecto a campos timestamps

--- database/migrations/2020_02_18_233009_create_ciudades_table.php
+++ database/migrations/2020_02_18_233009_create_ciudades_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateCiudadesTable extends Migration
 {
@@ -18,7 +19,8 @@ class CreateCiudadesTable extends Migration
             $table->unsignedInteger('codigo');
             $table->string('nombre', 100);
             $table->unsignedSmallInteger('departamento_id');
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
 
             $table->foreign('departamento_id')->references('id')->on('departamentos');
         });
--- database/migrations/2020_02_18_233112_create_usuarios_campania_table.php
+++ database/migrations/2020_02_18_233112_create_usuarios_campania_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateUsuariosCampaniaTable extends Migration
 {
@@ -26,7 +27,8 @@ class CreateUsuariosCampaniaTable extends Migration
             $table->string('correo')->nullable();
             $table->unsignedInteger('ciudad_id');
             $table->boolean('habeas_data')->default(false);
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
 
             $table->foreign('ciudad_id')->references('id')->on('ciudades');
         });
