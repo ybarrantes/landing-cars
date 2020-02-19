9dee8d2ff4d323009a8007bc06b0c2b633b02292
Yonathan Gustavo Barrantes Leon <ybarrantes.juntos085@gmail.com>
2020-02-18 22:58:37 -0500

    crud concurso

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,7 @@ Route::group(['prefix' => 'departamentos'], function()
 // rutas de ciudades
 Route::get('ciudades', 'CiudadController@index');
 Route::get('ciudades/{id}', 'CiudadController@show');
+
+// ruta para registrar usuarios del concurso
+Route::post('concurso/usuario/registrar', 'UsuarioConcursoController@store');
+Route::get('concurso/usuario/ganador', 'UsuarioConcursoController@ganador');
