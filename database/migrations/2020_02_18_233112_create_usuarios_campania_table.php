5f35b293425acbc336289f60f1e9c46c5a8d51b6
Yonathan Gustavo Barrantes Leon <ybarrantes.juntos085@gmail.com>
2020-02-18 19:43:39 -0500

    ajustar tabla a requerimiento

--- database/migrations/2020_02_18_233112_create_usuarios_campania_table.php
+++ database/migrations/2020_02_18_233112_create_usuarios_campania_table.php
@@ -17,13 +17,13 @@ class CreateUsuariosCampaniaTable extends Migration
         Schema::create('usuarios_campania', function (Blueprint $table) {
             $table->bigIncrements('id');
             // TODO: revisar tipo de dato: considero que debe ser de tipo string ya que podrian
-            //       existir registros de personas extranjeras; por esta razon prefiero realizar
-            //       el control de la validacion en el modelo y no dejar cerrado el campo en la base de datos
-            $table->string('cedula', 20)->unique();
+            //       existir registros de personas extranjeras, sin embargo se deja segun requerimiento
+            $table->unsignedBigInteger('cedula')->unique();
             $table->string('nombre');
             $table->string('apellido');
-            // TODO: revisar tipo de dato
-            $table->string('celular', 20);
+            // TODO: revisar tipo de dato: considero que debe ser de tipo string
+            //       sin embargo se deja segun requerimiento
+            $table->unsignedBigInteger('celular');
             $table->string('correo')->nullable();
             $table->unsignedInteger('ciudad_id');
             $table->boolean('habeas_data')->default(false);
